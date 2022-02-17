b68755d12c376e94d34a169bcfc6688f24249c2d
fionahenley <fionalove77@gmail.com>
2022-02-17 10:43:37 -0700

    adding base index

--- index.js
+++ index.js
@@ -1,13 +1,87 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require('inquirer');
+const generateMarkdown = require("./generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//array of questions for user 
+//const questions = [];
+function askQuestion(){
+inquirer.prompt([
+    {
+        type: "",
+        message: "",
+        name: ""
+    },
+    {
+        type: "",
+        message: "",
+        name: ""
+    },
+    {
+        type: "",
+        message: "",
+        name: ""
+    },
+    {
+        type: "",
+        message: "",
+        name: ""
+    },
+    {
+        type: "",
+        message: "",
+        name: ""
+    },
+    {
+        type: "",
+        message: "",
+        name: ""
+    },
+    {
+        type: "",
+        message: "",
+        name: ""
+    },
+    {
+        type: "",
+        message: "",
+        name: ""
+    },
+    
+]).then(function(answers){
+    var strReadme = generateMarkdown(answers);
+    writeToFile(answers, strReadme)
+})
+
+}
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    console.log(response)
+    fs.writeFile ('package.json' , JSON.stringify(response), err => (err)) 
+    } catch (err) {
+        console.error(err)
+    }
+
+
+
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    askQuestion()
+}
+
+
+
+
+
 
 // Function call to initialize app
-init();
+init();{
+
+}
