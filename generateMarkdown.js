25fecb8ec2cdbf68bd99d519e686812396fc8512
fionahenley <fionalove77@gmail.com>
2022-02-22 18:06:35 -0800

    updating code

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,84 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+let licenseURL;
+let licenseImg;
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function getLicense(answers) {
+  if (answers.promptFileUserLicense === "Apache License 2.0") {
+    licenseURL = "https://www.apache.org/licenses/LICENSE-2.0";
+    licenseImg = "Apache-brightgreen";
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+} else if (answers.promptFileUserLicense === "GNU General Public License v3.0") {
+    licenseURL = "https://www.gnu.org/licenses/gpl-3.0.en.html";
+    licenseImg = "GNUv3-green";
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+} else if (answers.promptFileUserLicense === "MIT License") {
+    licenseURL = "https://opensource.org/licenses/MIT";
+    licenseImg = "MIT-yellowgreen";
 
-`;
+} else if (answers.promptFileUserLicense === "BSD 2-Clause License") {
+    licenseURL = "https://opensource.org/licenses/BSD-2-Clause";
+    licenseImg = "BSD2-yellow";
+
+} else if (answers.promptFileUserLicense === "BSD 3-Clause License") {
+    licenseURL = "https://opensource.org/licenses/BSD-3-Clause";
+    licenseImg = "BSD3-yellow";
+
+} else if (answers.promptFileUserLicense === "Boost Software License 1.0") {
+    licenseURL = "https://www.boost.org/LICENSE_1_0.txt";
+    licenseImg = "Boost-orange";
+
+} else if (answers.promptFileUserLicense === "Creative Commons Zero v1.0 Universal") {
+    licenseURL = "https://creativecommons.org/publicdomain/zero/1.0/legalcode";
+    licenseImg = "CCZero-red";
+
+} else if (answers.promptFileUserLicense === "Eclipse Public License 2.0") {
+    licenseURL = "https://www.eclipse.org/legal/epl-2.0/";
+    licenseImg = "Eclipse-blue";
+
+} else if (answers.promptFileUserLicense === "GNU Affero General Public License v3.0") {
+    licenseURL = "https://www.gnu.org/licenses/agpl-3.0.en.html";
+    licenseImg = "GNUAffero-green";
+
+} else if (answers.promptFileUserLicense === "GNU General Public License v2.0") {
+    licenseURL = "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html";
+    licenseImg = "GNUv2-green";
+
+} else if (answers.promptFileUserLicense === "GNU Lesser Public License v2.1") {
+    licenseURL = "https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html";
+    licenseImg = "GNULesser-green";
+
+} else if (answers.promptFileUserLicense === "Mozilla Public License 2.0") {
+    licenseURL = "https://www.mozilla.org/en-US/MPL/2.0/";
+    licenseImg = "MozillaPublic-blueviolet";
+
+} else {
+    licenseURL = "https://unlicense.org/";
+    licenseImg = "Unlicense-lightgrey"
+};
 }
 
+// function to generate markdown for README
+function generateMarkdown(answers) {
+  getLicense(answers)
+
+  return `# ${answers.promptFileName}`
+}
+
+
+## Table of Contents
+1. [Description](this.#description)
+2. [Installation](this.#installation)
+3. [Usage](this.#usage)
+4. [Contact](this.#contact)
+
+### Description
+
+{answers.promptprojectDescription}
+
+
+
+
+
+
+
 module.exports = generateMarkdown;
+
--- index.js
+++ index.js
@@ -1,87 +1,97 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
 const fs = require("fs");
 const inquirer = require('inquirer');
-const generateMarkdown = require("./generateMarkdown");
+const generateMarkdown = require("./generateMarkdown.js");
 
-// TODO: Create an array of questions for user input
-//array of questions for user 
-//const questions = [];
-function askQuestion(){
-inquirer.prompt([
+//questions for user
+console.log("hi")
+ inquirer.prompt([
     {
-        type: "",
-        message: "",
-        name: ""
+        type: "input",
+        name: "promptUsername",
+        message: "Hi, Welcome! What is your name?"
+        
     },
     {
-        type: "",
-        message: "",
-        name: ""
+        type: "input",
+        name: "promptUserEmail",
+        message: "Nice to meet you. What is your preferred email for contact?"
+        
     },
     {
-        type: "",
-        message: "",
-        name: ""
+        type: "input",
+        name: "promptUserGithub",
+        message: "Great! What is your Github Username?"
+       
     },
     {
-        type: "",
-        message: "",
-        name: ""
+        type: "input",
+        name: "promptUserRepo",
+        message: "What will the name of your Repository be called?"
+       
     },
     {
-        type: "",
-        message: "",
-        name: ""
+        type: "input",
+        name: "promptFilename",
+        message: "What will the name of your File be called?"
+       
     },
     {
-        type: "",
-        message: "",
-        name: ""
+        type: "input",
+        name: "promptPropjectDescription",
+        message: "What is your project about?"
+        
     },
     {
-        type: "",
-        message: "",
-        name: ""
+        type: "list",
+        name: "promptUserFileLicense" ,
+        message: "Lets apply the appropriate licensing for your project. Using arrow keys, you can select the license for your project. ",
+        choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause License", "BSD 3-Clause License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser Public License v2.1", "Mozilla Public License 2.0", "The Unlicense"]
     },
-    {
-        type: "",
-        message: "",
-        name: ""
-    },
+  
     
-]).then(function(answers){
-    var strReadme = generateMarkdown(answers);
-    writeToFile(answers, strReadme)
-})
+ ])
+ .then((answers) => {
+    console.log(answers);
+ });
 
+//function to write README file
+ function writeToFile(fileName, data) {
+    return fs.writeFileSync(fileName, data)
 }
 
 
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    console.log(response)
-    fs.writeFile ('package.json' , JSON.stringify(response), err => (err)) 
-    } error();(err) => {
-        console.error(err)
-    }
-
-
-
-
-
-
-// TODO: Create a function to initialize app
+// function to initialize program
 function init() {
-    askQuestion()
-}
+ inquirer.prompt(questions).then(answers => {
+    console.log(JSON.stringify(answers))
+ })}
+
+// //function call to initialize program
+ init();
 
 
 
 
 
+
+// function to write README file
+// function writeToFile(fileName, data) {
+//     return fs.writeFileSync(fileName, data)
+// }
+//function writeToFile(fileName, data) {
+    //console.log(response)
+    //fs.writeFile ('README.md' , JSON.stringify(response), err => (err)) 
+    //} error();(err) => {
+    //    console.error(err)
+   // }
+
+//function to initialize app
+//function init() {
+   // askQuestion()
+    //writeToFile("./README.md", )
+    //console.log("")
+//}
 
 // Function call to initialize app
-init();{
-
-}
+ //init();
